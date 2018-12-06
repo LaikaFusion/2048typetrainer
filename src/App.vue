341a514c6228320fa18ed1287b2897f52489fedc
LaikaFusion <newrad4@gmail.com>
2018-12-05 20:05:44 -0500

    game is done for now

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="app">
     <Title/>
-    <Gameboard @newgame="newGame" :score=score :winPow=winPow :pieceVal=pieceVal :selectedKeys=selectedKeys :grid=grid :gridsize=gridsize />
+    <Gameboard
+      @newgame="newGame"
+      :score="score"
+      :winPow="winPow"
+      :pieceVal="pieceVal"
+      :selectedKeys="selectedKeys"
+      :grid="grid"
+      :gridsize="gridsize"
+    />
+    <br>
+    <div>Instructions: Use the key shown in the white circle to move the blocks that direction. Blocks combine with like blocks. You're trying to get to 2048.</div>
+    <br>
+    <div>Based on <a href="https://play2048.co/">2048</a></div>
+    <modal name="win"><div class='winnerTitle'><div>A Winner Is You!</div></div></modal>
+    <modal name="lose"><div class='winnerTitle'><div>Do you want to play the game again?</div></div></modal>
+
   </div>
 </template>
 
 <script>
 import Title from "./components/Title.vue";
 import Gameboard from "./components/Gameboard.vue";
-
 export default {
   name: "app",
   components: {
@@ -24,22 +38,41 @@ export default {
       winPow: 11,
       pieceVal: 2,
       gameOver: false,
-      validKeys: "|`~-_=+{[]};:',<>/\\".split(''),
+      validKeys: "|`~-_=+{[]};:',<>/\\".split(""),
       selectedKeys: []
     };
   },
   methods: {
-    selectKeys: function(keyToReplace="none"){
-      if(this.selectedKeys.length === 0){
+    show: function() {
+    this.$modal.show("lose");
+  },
+  hide: function() {
+    this.$modal.hide("win");
+  },
+    selectKeys: function(keyToReplace = "none") {
+      if (this.selectedKeys.length === 0) {
         for (let index = 0; index < 4; index++) {
-          let item = this.validKeys.splice(Math.floor(Math.random()*this.validKeys.length),1)
+          let item = this.validKeys.splice(
+            Math.floor(Math.random() * this.validKeys.length),
+            1
+          );
           this.selectedKeys.push(item[0]);
         }
-        return
+        return;
       }
       //You need to replace the whole array or vue doesn't count it as updated so just splice won't work
       let ArrCopy = this.selectedKeys.slice();
-      ArrCopy.splice(ArrCopy.indexOf(keyToReplace),1, this.validKeys.splice(Math.floor(Math.random()*this.validKeys.length),1,keyToReplace).toString());
+      ArrCopy.splice(
+        ArrCopy.indexOf(keyToReplace),
+        1,
+        this.validKeys
+          .splice(
+            Math.floor(Math.random() * this.validKeys.length),
+            1,
+            keyToReplace
+          )
+          .toString()
+      );
       this.selectedKeys = ArrCopy;
     },
     createZeroBoard: function(gridsize) {
@@ -50,170 +83,183 @@ export default {
       this.direction = "";
       this.score = 0;
       this.gameOver = false;
-      this.validKeys= "|`~-_=+{[]};:',<>/\\".split(''),
-      this.selectedKeys=[];
+      (this.validKeys = "|`~-_=+{[]};:',<>/\\".split("")),
+        (this.selectedKeys = []);
       this.selectKeys();
       this.generateRandomSpot();
       this.generateRandomSpot();
     },
     generateRandomSpot: function() {
-      if(this.containsZeros()){
-         const emptySpots = this.grid
-        .map((e, i) => (e === 0 ? i : ""))
-        .filter(Number);
-      const randomItem =
-        emptySpots[Math.floor(Math.random() * emptySpots.length)];
-      this.grid[randomItem] = Math.random() < 0.9 ? 1 : 2;
+      if (this.containsZeros()) {
+        const emptySpots = this.grid
+          .map((e, i) => (e === 0 ? i : ""))
+          .filter(Number);
+        const randomItem =
+          emptySpots[Math.floor(Math.random() * emptySpots.length)];
+        this.grid[randomItem] = Math.random() < 0.9 ? 1 : 2;
       }
-     
     },
     movement: function(key) {
-      let newArray = []
-      if(this.direction==="left"||this.direction==="right"){for (let i = 0; i < (this.gridsize); i++) {
-
-        let row = this.grid.slice((i*this.gridsize),(i*this.gridsize)+this.gridsize);
-        if(this.direction==="right"){
-          row = row.reverse()
-        }
-        row = this.rowReduceLeft(row);
-        if(this.direction==="right"){
-          row = row.reverse()
-        }
-        newArray = newArray.concat(row);
-      }}
-      if(this.direction==="up"||this.direction==="down"){
-        newArray = this.createZeroBoard(this.gridsize);
-        for (let i = 0; i < (this.gridsize); i++) {
-          let row = this.grid.filter((e,loc)=>{
-            if ((loc+this.gridsize) % this.gridsize===i){
-              return true
-            }
-          })         
-          if(this.direction==="down"){
-            row = row.reverse()
+      let newArray = [];
+      if (this.direction === "left" || this.direction === "right") {
+        for (let i = 0; i < this.gridsize; i++) {
+          let row = this.grid.slice(
+            i * this.gridsize,
+            i * this.gridsize + this.gridsize
+          );
+          if (this.direction === "right") {
+            row = row.reverse();
           }
           row = this.rowReduceLeft(row);
-          if(this.direction==="down"){
-            row = row.reverse()
+          if (this.direction === "right") {
+            row = row.reverse();
+          }
+          newArray = newArray.concat(row);
+        }
+      }
+      if (this.direction === "up" || this.direction === "down") {
+        newArray = this.createZeroBoard(this.gridsize);
+        for (let i = 0; i < this.gridsize; i++) {
+          let row = this.grid.filter((e, loc) => {
+            if ((loc + this.gridsize) % this.gridsize === i) {
+              return true;
+            }
+          });
+          if (this.direction === "down") {
+            row = row.reverse();
+          }
+          row = this.rowReduceLeft(row);
+          if (this.direction === "down") {
+            row = row.reverse();
           }
           for (let index = 0; index < this.gridsize; index++) {
-            newArray[i+(index*this.gridsize)] = row[index]
+            newArray[i + index * this.gridsize] = row[index];
           }
         }
       }
-      if(this.grid.toString() !== newArray.toString()){
+      if (this.grid.toString() !== newArray.toString()) {
         this.grid = newArray;
         this.generateRandomSpot();
-        this.selectKeys(key)
-
+        this.selectKeys(key);
       }
 
-      this.gameEndCheck()
+      this.gameEndCheck();
     },
-    gameEndCheck: function(){
-      if(!this.containsZeros() && !this.containsMatchs()){
+    gameEndCheck: function() {
+      if (!this.containsZeros() && !this.containsMatchs()) {
         this.gameOver = true;
+        this.$modal.show("lose");
+
         console.log("Lose");
         return;
       }
-      if(this.grid.includes(this.winPow)){
+      if (this.grid.includes(this.winPow)) {
         this.gameOver = true;
-        console.log("Win")
+        this.$modal.show("win");
+        console.log("Win");
         return;
       }
     },
-    containsZeros: function(){
-      return this.grid.includes(0)
+    containsZeros: function() {
+      return this.grid.includes(0);
     },
-    containsMatchs: function(){
-      let returnVal = false
-      this.grid.forEach((e,i)=>{
-        if(e!==0){
-          if (this.grid[i+1]===e||this.grid[i-1]===e||this.grid[i+(this.gridsize)]===e||this.grid[i-(this.gridsize)]===e){
-           returnVal =  true
+    containsMatchs: function() {
+      let returnVal = false;
+      this.grid.forEach((e, i) => {
+        if (e !== 0) {
+          if (
+            this.grid[i + 1] === e ||
+            this.grid[i - 1] === e ||
+            this.grid[i + this.gridsize] === e ||
+            this.grid[i - this.gridsize] === e
+          ) {
+            returnVal = true;
           }
         }
-      })
+      });
       return returnVal;
     },
     //This was fun to write. It will only work one way so make sure you hand it the array in the right direction. It moves every number to the left, then starts adding them. This emulates the behavior of the actual 2048 game
-    rowReduceLeft: function(rowArr){
-    let movesLeft= true;
-    do{
-      movesLeft=false
+    rowReduceLeft: function(rowArr) {
+      let movesLeft = true;
+      do {
+        movesLeft = false;
         rowArr.forEach((e, i) => {
-          if(i !==0){
-             if (
-            (rowArr[i - 1] === 0 ) && e !==0
-          ) {
-            rowArr[i - 1] += e;
-            rowArr[i]=0
-            movesLeft=true;
+          if (i !== 0) {
+            if (rowArr[i - 1] === 0 && e !== 0) {
+              rowArr[i - 1] += e;
+              rowArr[i] = 0;
+              movesLeft = true;
+            }
           }
-          }
-         
         });
-    }while(movesLeft)
-    rowArr.forEach((e,i)=>{
-      if(i!==0){
-        if(rowArr[i-1]===e&&e!==0){
-          rowArr[i - 1] += 1;
-          rowArr.splice(i,1)
-          rowArr.push(0)
-          this.score +=Math.pow(2,e)*(.5*this.pieceVal)
+      } while (movesLeft);
+      rowArr.forEach((e, i) => {
+        if (i !== 0) {
+          if (rowArr[i - 1] === e && e !== 0) {
+            rowArr[i - 1] += 1;
+            rowArr.splice(i, 1);
+            rowArr.push(0);
+            this.score += Math.pow(2, e) * (0.5 * this.pieceVal);
+          }
         }
-      }
-    })
-    return rowArr; 
-  },
-  
+      });
+      return rowArr;
+    }
   },
   beforeMount() {
     this.newGame();
   },
   mounted() {
     window.addEventListener("keydown", e => {
-      // if(e.key===this.selectedKeys[0]){
-      //   this.direction = "up";
-      // }
-      // else if (e.key===this.selectedKeys[1]){
-      //   this.direction = "down";
-      // }
-      // else if (e.key ===this.selectedKeys[2]){
-      //   this.direction = "right";      
-      // }
-      // else if (e.key === this.selectedKeys[3]){
-      //   this.direction = "left";
-      // }
-            if(e.key==="w"){
+      if(e.key===this.selectedKeys[0]){
         this.direction = "up";
       }
-      else if (e.key==="s"){
+      else if (e.key===this.selectedKeys[1]){
         this.direction = "down";
       }
-      else if (e.key ==="d"){
-        this.direction = "right";      
+      else if (e.key ===this.selectedKeys[2]){
+        this.direction = "right";
       }
-      else if (e.key === "a"){
+      else if (e.key === this.selectedKeys[3]){
         this.direction = "left";
       }
-      else{
+      else {
         return;
       }
-      this.movement(e.key)
+      this.movement(e.key);
     });
   },
+  
 };
 </script>
 
 <style>
-html{
-  background-color:lightgray;
+html {
+  background-color: lightgray;
 }
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
+.winnerTitle{
+  display: flex;
+  font-size:64px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  color:white;
+      border: 15px grey solid;
+  border-radius: 6px;
+  box-sizing: border-box;
+  text-align: center;
+  padding: 10px;
+}
+.v--modal {
+  background-color: rgba(0, 0, 0, 0) !important;
 }
 </style>
--- src/components/Gameboard.vue
+++ src/components/Gameboard.vue
@@ -2,24 +2,33 @@
 <div class="gameArea">
   <div class="row">
     <div class="score">Score:{{score}}</div>
-    <div class="keyStyle">{{selectedKeys[0]}}</div>
-    <div v-on:click="$emit('newgame')" class="newGameButton">New Game</div>
+   
+    <div v-on:click="$emit('newgame')" class="newGameButton"><span>New Game</span></div>
   </div>
-  <div class= "row">
+    <div class="row">
+
+  <div class="gamecontainer">
+   
+    <div class="keyStyle">{{selectedKeys[0]}}</div>
+    
+    <div class='middle'>
     <div class="keyStyle">{{selectedKeys[3]}}</div>    
     <div :style="style" class="gameboard">
       <div v-for="(cell,index) in grid" :key=index>
         <Tile :pieceVal=pieceVal :winPow=winPow v-bind:number=cell />
       </div>
     </div>
-    <div class="keyStyle">{{selectedKeys[2]}}</div>    
+        <div class="keyStyle">{{selectedKeys[2]}}</div>  
+
+    </div>
+        <div class="keyStyle">{{selectedKeys[1]}}</div>
+  
   </div>
-   <div class="row">
-     <div class="hiddenDiv"> </div>
-    <div class="keyStyle">{{selectedKeys[1]}}</div>
-    <div class="hiddenDiv"> </div>
   </div>
-</div>
+ 
+  </div>
+  
+   
 
 </template>
 
@@ -57,31 +66,29 @@ export default {
 .gameboard {
   background-color: grey;
   display: grid;
-  height: 600px;
-  width: 600px;
+  height: 60vh;
+  width: 60vh;
   grid-gap: 2%;
-  padding: 2%;
+  padding: 1em;
   border-radius: 6px;
 }
 .row{
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  width: 70%;
   margin: auto;
 
 }
 .keyStyle{
-  margin: 10px;
-  font-size: 40px;
-  background-color: white;
-  width: 100px;
-  height: 100px;
+  font-size: 35px;
+  height: 1.4em;
+  width: 1.4em;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 20px grey solid;
-  border-radius: 6px;
+  background-color:white;
+  border-radius: 1em;
+
 }
 .score{
   width: 150px;
@@ -91,13 +98,32 @@ export default {
   font-size:30px;
   background-color: black;
   width: 150px;
+  height: 3em;
   border: 10px grey solid;
   border-radius: 6px;
   color:white;
   box-sizing: border-box;
   cursor: pointer;
+  text-align: center;
+  margin-left: 1em;
+  
 }
 .hiddenDiv{
   width:150px
+}
+.gamecontainer{
+  background-color:gray;
+
+  display:flex;
+  flex-direction: column;
+  align-items:center;
+  margin-top:2rem;
+  padding: 1em;
+  border-radius: 6px;
+}
+.middle{
+  display:flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
